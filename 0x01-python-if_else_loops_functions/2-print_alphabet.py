0c42e16899a31b35fc652be0101c040f687f6ace
Younisprogramming <younistaher12341@gmail.com>
2023-10-31 16:52:15 -0700

    task 2 done {print print_alphabet}

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        print("{:c}".format(ch), end='')
+for char_code in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(char_code)), end='')
