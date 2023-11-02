1cbdcaf5bf6a414c80b506ffee8930a1ac638417
Younisprogramming <younistaher12341@gmail.com>
2023-11-02 05:43:46 -0700

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv)
     if (n == 1):
-        print("0 arguments.")
+        print("{} arguments.".format(n - 1))
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):
