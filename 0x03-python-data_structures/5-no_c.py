ca453111027943b190219fe4143da98dbc3474db
Younisprogramming <younistaher12341@gmail.com>
2023-11-06 08:24:57 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,5 @@ def no_c(my_string):
     new_string = my_string[:]
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-                new_string = new_string[:i] + my_string[i+1:]
-
+            new_string = new_string[:i] + my_string[i+1:]
     return new_string
