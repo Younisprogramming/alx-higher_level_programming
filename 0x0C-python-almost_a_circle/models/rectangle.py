455db901803f0ab28feeff4d804db7ec7a978b8f
Younisprogramming <younistaher12341@gmail.com>
2023-12-08 16:12:59 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ __init__ func doc """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -21,36 +21,45 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """ func2 """
+        def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """ fun3 """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ func4 """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        """ func5 """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ func6 """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """ func7 """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ func8 """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
