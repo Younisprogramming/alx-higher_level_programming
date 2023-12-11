2bda68768c482cca3c5fb16dc3b5a14cddea9fc1
Younisprogramming <younistaher12341@gmail.com>
2023-12-11 13:58:14 -0800

    dawdaw

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,24 @@ class Square(Rectangle):
     def __str__(self):
         """ str method """
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
+
+    def update(self, *args, **kwargs):
+        """ijijjk ijjk"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
