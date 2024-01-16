e89be5e3d09d7f8ea35d07c5fc62c0a756952e45
Younisprogramming <younistaher12341@gmail.com>
2024-01-16 14:52:31 -0800

    task1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-CREATE databases if not exists hbtn_0c_0;
+-- dwdawdmwakldmlw 
+-- dwdawdwdjwndwj
+CREATE database if not exists hbtn_0c_0;
