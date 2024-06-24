7de0c65a0c2aa96b2bb83ee2dc74a62ec9eaceea
younisprogramming <younistaher12341@gmail.com>
2024-06-23 18:51:52 -0700

    hi

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  hirw iutw stohst i"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.name\
+                FROM cities\
+                JOIN states\
+                ON cities.state_id = states.id\
+                WHERE states.name=%s ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join(city[0] for city in rows))
+    cur.close()
+    db.close()
